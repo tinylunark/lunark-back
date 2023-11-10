a0a77f3a7f068cb03825503112eb90fc02939862
Vladislav Radovic <vradovic522@gmail.com>
2023-11-10 21:14:26 +0100

    Bootstrap project

---
+++ src/main/java/com/lunark/lunark/LunarkApplication.java
@@ -0,0 +1,13 @@
+package com.lunark.lunark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunarkApplication.class, args);
+	}
+
+}
