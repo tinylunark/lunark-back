1861d8f650470681d7c835d817b64801a72e7443
Momir Milutinović <milutinm462@gmail.com>
2023-11-22 19:19:40 +0100

    feat: Add authentication parameters to reservation endpoints

--- src/main/java/com/lunark/lunark/controller/ReservationController.java
+++ src/main/java/com/lunark/lunark/controller/ReservationController.java
@@ -16,35 +16,35 @@ import java.util.Collection;
 @RequestMapping("/api/reservations")
 public class ReservationController {
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ReservationConfirmationDto> createReservation(@RequestBody ReservationRequestDTO reservationRequestDTO) {
+    public ResponseEntity<ReservationConfirmationDto> createReservation(@RequestHeader("x-access-token") String guestToken, @RequestBody ReservationRequestDTO reservationRequestDTO) {
         ReservationConfirmationDto reservationConfirmationDto = new ReservationConfirmationDto("Vila Golija", LocalDate.of(2024, 6, 14), LocalDate.of(2023, 6, 16), 9959, 3);
         return new ResponseEntity<>(reservationConfirmationDto, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{reservation_id}")
-    public ResponseEntity<ReservationDto> getReservation(@PathVariable("reservation_id") Long Id) {
+    public ResponseEntity<ReservationDto> getReservation(@RequestHeader("x-access-token") String token, @PathVariable("reservation_id") Long Id) {
         ReservationDto reservationDto = new ReservationDto("Vila Golija", LocalDate.of(2024, 6, 14), LocalDate.of(2023, 6, 16), 9959, 1L, 3);
         return new ResponseEntity<>(reservationDto, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{reservation_id}")
-    public ResponseEntity<ReservationDto> deleteReservation(@PathVariable("reservation_id") Long Id) {
+    public ResponseEntity<ReservationDto> deleteReservation(@RequestHeader("x-access-token") String guestToken, @PathVariable("reservation_id") Long Id) {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping(value = "/accept/{reservation_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> acceptReservation(@PathVariable("reservation_id") Long id) {
+    public ResponseEntity<String> acceptReservation(@RequestHeader("x-access-token") String hostToken, @PathVariable("reservation_id") Long id) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping(value = "/reject/{reservation_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> rejectReservation(@PathVariable("reservation_id") Long id) {
+    public ResponseEntity<String> rejectReservation(@RequestHeader("x-access-token") String hostToken, @PathVariable("reservation_id") Long id) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
     @GetMapping(value="")
-    public ResponseEntity<Collection<ReservationDto>> getReservations(@RequestHeader("Cookie") String cookie, @RequestParam(value = "propertyName", required = false) String propertyName, @RequestParam(value = "date", required = false) LocalDate date, @RequestParam(value = "status", required = false) String status){
+    public ResponseEntity<Collection<ReservationDto>> getReservations(@RequestHeader("x-access-token") String token, @RequestParam(value = "propertyName", required = false) String propertyName, @RequestParam(value = "date", required = false) LocalDate date, @RequestParam(value = "status", required = false) String status){
         ArrayList<ReservationDto> reservationDtos = new ArrayList<>();
         reservationDtos.add(new ReservationDto("Hotel Oderberger", LocalDate.of(2024, 7, 14), LocalDate.of(2023, 7, 16), 15000, 1L, 1));
 
