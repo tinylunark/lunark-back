cc18bd28aa0e55c0caaab3ac14e990329dd24206
Momir Milutinović <milutinm462@gmail.com>
2023-11-22 18:08:42 +0100

    feat: Delete reservation endpoint

--- src/main/java/com/lunark/lunark/controller/ReservationController.java
+++ src/main/java/com/lunark/lunark/controller/ReservationController.java
@@ -24,4 +24,19 @@ public class ReservationController {
         ReservationDto reservationDto = new ReservationDto("Vila Golija", LocalDate.of(2024, 6, 14), LocalDate.of(2023, 6, 16), 9959, 1L);
         return new ResponseEntity<>(reservationDto, HttpStatus.OK);
     }
+
+    @DeleteMapping(value = "/{reservation_id}")
+    public ResponseEntity<ReservationDto> deleteReservation(@PathVariable("reservation_id") Long Id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping(value = "/accept/{reservation_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> acceptReservation(@PathVariable("reservation_id") Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/reject/{reservation_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> rejectReservation(@PathVariable("reservation_id") Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
