5078a75ffaabbfe7f031941d0bcf9c88ee39eefe
Momir Milutinović <milutinm462@gmail.com>
2023-11-29 21:57:28 +0100

    feat: Add dto for property reports

--- src/main/java/com/lunark/lunark/controller/PropertyReportController.java
+++ src/main/java/com/lunark/lunark/controller/PropertyReportController.java
@@ -1,9 +1,13 @@
 package com.lunark.lunark.controller;
 
+import com.lunark.lunark.dto.PropertyReportDto;
 import org.modelmapper.internal.bytebuddy.implementation.Implementation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/property-report")
@@ -12,13 +16,18 @@ public class PropertyReportController {
     @GetMapping(value = "/download", produces =  "application/json")
     public ResponseEntity<String> downloadRecord(@RequestParam Long propertyId, @RequestParam String startDate, @RequestParam String endDate) {
         // TODO: Implementation
-       return ResponseEntity.ok("Dummy string.");
+
+       return ResponseEntity.ok("Dummy pdf.");
     }
 
     @GetMapping("/")
-    public ResponseEntity<String> sendRecordData(@RequestParam Long propertyId, @RequestParam String startDate, @RequestParam String endDate) {
-        String response = String.format("Record data received: PropertyId=%d, StartDate=%s, EndDate=%s",
-                propertyId, startDate, endDate);
-        return ResponseEntity.ok().body(response);
+    public ResponseEntity<PropertyReportDto> sendRecordData(@RequestParam Long propertyId, @RequestParam String startDate, @RequestParam String endDate) {
+        PropertyReportDto propertyReportDto = new PropertyReportDto(
+                120000.0,
+                120,
+                Arrays.asList(10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0),
+                Arrays.asList(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+        );
+        return new ResponseEntity<>(propertyReportDto, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/lunark/lunark/dto/PropertyReportDto.java
@@ -0,0 +1,17 @@
+package com.lunark.lunark.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PropertyReportDto {
+    private Double totalProfit;
+    private Integer totalReservations;
+    private List<Double> profitByMonth;
+    private List<Integer> reservationsByMonth;
+}
