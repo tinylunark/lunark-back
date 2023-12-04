5e8622dec29a935e6665cea8f7a0a5be8fc20cd8
Momir Milutinović <milutinm462@gmail.com>
2023-12-04 14:52:48 +0100

    refactor: Rename property service to be in accordance with new naming scheme

--- src/main/java/com/lunark/lunark/service/IPropertyService.java
+++ src/main/java/com/lunark/lunark/service/IPropertyService.java
@@ -5,6 +5,7 @@ import com.lunark.lunark.model.PropertyImage;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+
 import java.util.Collection;
 import java.util.Optional;
 
