1c5f99028885ab0738f3185654a0000e62809b04
Vladislav Radovic <vradovic522@gmail.com>
2023-12-06 17:16:41 +0100

    feat: remove redundant image icon

--- src/main/java/com/lunark/lunark/model/PropertyImage.java
+++ src/main/java/com/lunark/lunark/model/PropertyImage.java
@@ -2,16 +2,9 @@ package com.lunark.lunark.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcType;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.Type;
 import org.hibernate.type.descriptor.jdbc.VarbinaryJdbcType;
-import org.springframework.data.jpa.repository.Query;
-
-import javax.swing.*;
 
 @Entity
 @Data
@@ -26,9 +19,5 @@ public class PropertyImage {
     @JdbcType(VarbinaryJdbcType.class)
     @JsonIgnore
     byte[] imageData;
-    @Lob
-    @JdbcType(VarbinaryJdbcType.class)
-    @JsonIgnore
-    ImageIcon image;
     String mimeType;
 }
