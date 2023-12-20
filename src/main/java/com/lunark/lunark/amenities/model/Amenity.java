4d580012b967875d3f429d57864ebcd524af49bf
Momir Milutinović <milutinm462@gmail.com>
2023-12-20 21:08:08 +0100

    feat: Soft delete amenity

--- src/main/java/com/lunark/lunark/amenities/model/Amenity.java
+++ src/main/java/com/lunark/lunark/amenities/model/Amenity.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import java.awt.*;
 import java.util.Collection;
@@ -14,6 +16,11 @@ import java.util.Collection;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@SQLDelete(sql
+        = "UPDATE amenity "
+        + "SET deleted = true "
+        + "WHERE id = ?")
+@Where(clause = "deleted = false")
 public class Amenity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +31,15 @@ public class Amenity {
 
     //Material design icon name
     private String icon;
+
+    @Column(name = "deleted", columnDefinition = "boolean default false")
+    private boolean deleted = false;
+
+    public Amenity(Long id, String name, String icon) {
+        this.id = id;
+        this.name = name;
+        this.icon = icon;
+    }
 
     @Override
     public String toString() {
