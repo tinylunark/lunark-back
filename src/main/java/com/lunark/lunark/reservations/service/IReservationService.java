3badef84a05cbf3a7803c91e8c236422a81d3ae0
dusanlecic <dusan.lecic5@gmail.com>
2023-12-20 21:31:33 +0100

    feat: imeplemnt two new methods

--- src/main/java/com/lunark/lunark/reservations/service/IReservationService.java
+++ src/main/java/com/lunark/lunark/reservations/service/IReservationService.java
@@ -1,8 +1,15 @@
 package com.lunark.lunark.reservations.service;
 
+import com.lunark.lunark.properties.model.Property;
 import com.lunark.lunark.reservations.dto.ReservationRequestDto;
 import com.lunark.lunark.reservations.model.Reservation;
 
+import java.util.List;
+
 public interface IReservationService {
     Reservation create(ReservationRequestDto reservation, String username);
+
+    List<Reservation> getAllReservationsForPropertiesList(List<Property> propertiesList);
+
+    List<Reservation> getAllReservationsForUser(Long userId);
 }
