2dd2ddd0339f1382fb49bde7d13c8d6da4d99eae
Vladislav Radovic <vradovic522@gmail.com>
2023-12-26 17:21:39 +0100

    feat: add latitude and longitude fields to property request dto

--- src/main/java/com/lunark/lunark/properties/dto/PropertyRequestDto.java
+++ src/main/java/com/lunark/lunark/properties/dto/PropertyRequestDto.java
@@ -25,4 +25,6 @@ public class PropertyRequestDto {
     Collection<AvailabilityEntryDto> availabilityEntries;
     Property.PropertyType type;
     Long hostId;
+    double latitude;
+    double longitude;
 }
