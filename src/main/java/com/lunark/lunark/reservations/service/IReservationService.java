00e622e0959505376d99b00de9552504154521fe
dusanlecic <dusan.lecic5@gmail.com>
2023-12-30 23:10:55 +0100

    feat: add some moderation methods for resevation

--- src/main/java/com/lunark/lunark/reservations/service/IReservationService.java
+++ src/main/java/com/lunark/lunark/reservations/service/IReservationService.java
@@ -5,11 +5,18 @@ import com.lunark.lunark.reservations.dto.ReservationRequestDto;
 import com.lunark.lunark.reservations.model.Reservation;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IReservationService {
     Reservation create(ReservationRequestDto reservation, String username);
 
+    Optional<Reservation> findById(Long id);
+
     List<Reservation> getAllReservationsForPropertiesList(List<Property> propertiesList);
 
     List<Reservation> getAllReservationsForUser(Long userId);
+
+    public List<Reservation> getIncomingReservationsForHostId(Long hostId);
+
+    void save(Reservation reservation);
 }
