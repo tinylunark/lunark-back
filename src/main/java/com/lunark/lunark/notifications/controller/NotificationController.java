234ef8e80ab045280e60b7261e696b3d0626fa58
Momir Milutinović <milutinm462@gmail.com>
2024-01-03 19:29:34 +0100

    feat: Send constant number of unread notifications after subscribing to user notification topic

--- src/main/java/com/lunark/lunark/notifications/controller/NotificationController.java
+++ src/main/java/com/lunark/lunark/notifications/controller/NotificationController.java
@@ -1,16 +1,33 @@
 package com.lunark.lunark.notifications.controller;
 
 import com.lunark.lunark.notifications.dto.NotificationResponseDto;
+import com.lunark.lunark.notifications.dto.UnreadNotificationCountDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/notifications")
 public class NotificationController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @EventListener
+    private void handleSessionSubscribed(SessionSubscribeEvent event) {
+        Map<String, String> message = new HashMap<>();
+        message.put("unreadNotificationCount", "5");
+        System.out.println("Sending message");
+        simpMessagingTemplate.convertAndSendToUser(event.getUser().getName(), "/socket-publisher",  message);
+    }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<NotificationResponseDto>> findAllForUser(@PathVariable("id") Long userId) {
