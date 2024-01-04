325d7c6f2bf9f19057ca592de634bd7ec1a82feb
dusanlecic <dusan.lecic5@gmail.com>
2024-01-04 20:25:42 +0100

    refactor: update review controller

--- src/main/java/com/lunark/lunark/moderation/controller/ReviewReportController.java
+++ src/main/java/com/lunark/lunark/moderation/controller/ReviewReportController.java
@@ -2,14 +2,18 @@ package com.lunark.lunark.moderation.controller;
 
 import com.lunark.lunark.moderation.dto.ReviewReportRequestDto;
 import com.lunark.lunark.moderation.dto.ReviewReportResponseDto;
-import com.lunark.lunark.reviews.service.ReviewReportService;
+import com.lunark.lunark.moderation.model.ReviewReport;
+import com.lunark.lunark.moderation.service.ReviewReportService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/reports/reviews")
@@ -17,28 +21,38 @@ public class ReviewReportController {
     @Autowired
     private ReviewReportService reviewReportService;
 
+    @Autowired
+    ModelMapper modelMapper;
+
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ReviewReportResponseDto> getReviewReport(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(new ReviewReportResponseDto(null, null, null, null), HttpStatus.OK);
+    public ResponseEntity<ReviewReport> getReviewReport(@PathVariable("id") Long id) {
+        ReviewReport reviewReport = reviewReportService.getById(id);
+        return reviewReport != null ? new ResponseEntity<>(reviewReport, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ReviewReportResponseDto>> getAll() {
-        return new ResponseEntity<>(List.of(), HttpStatus.OK);
+        List<ReviewReport> reviewReports = reviewReportService.getAll();
+        List<ReviewReportResponseDto> reportDtos = reviewReports.stream()
+                .map(report -> new ReviewReportResponseDto(report.getId(), report.getDate(), report.getReporter().getId(), report.getReview().getId()))
+                .collect(Collectors.toList());
+
+        return reviewReports.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(reportDtos, HttpStatus.OK);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ReviewReportResponseDto> createReviewReport(@RequestBody ReviewReportRequestDto dto) {
-        return new ResponseEntity<>(new ReviewReportResponseDto(null, null, null, null), HttpStatus.CREATED);
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ReviewReportResponseDto> updateReviewReport(@PathVariable Long id, @RequestBody ReviewReportRequestDto dto) {
-        return new ResponseEntity<>(new ReviewReportResponseDto(null, null, null, null), HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<ReviewReportResponseDto> deleteReviewReport(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteReviewReport(@PathVariable Long id) {
+        reviewReportService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
