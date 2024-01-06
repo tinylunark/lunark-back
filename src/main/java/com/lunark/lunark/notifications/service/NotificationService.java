64c74d06f945a8643a04da4c26b9c420c93d4d1a
Momir Milutinović <milutinm462@gmail.com>
2024-01-06 23:31:19 +0100

    fix: Fix database error when sending reservation related notifications

--- src/main/java/com/lunark/lunark/notifications/service/NotificationService.java
+++ src/main/java/com/lunark/lunark/notifications/service/NotificationService.java
@@ -115,11 +115,11 @@ public class NotificationService implements INotificationService {
     private String createReservationNotificationMessage(Reservation reservation) {
         switch (reservation.getStatus()) {
             case ACCEPTED:
-                return "Reservation at has been accepted.";
+                return "Reservation at " + reservation.getProperty().getName() + " has been accepted.";
             case REJECTED:
-                return "Reservation at has been rejected.";
+                return "Reservation at " + reservation.getProperty().getName() + " has been rejected.";
             case CANCELLED:
-                return "Reservation at has been cancelled." ;
+                return "Reservation at " + reservation.getProperty().getName() + " has been cancelled by " + reservation.getGuest().getName() + " " + reservation.getGuest().getSurname();
             default:
                 throw new IllegalArgumentException("Invalid reservation status");
         }
