9a464a4135fb620c6000386a0be18fa854e3fd06
Vladislav Radovic <vradovic522@gmail.com>
2024-01-11 19:43:32 +0100

    refactor: change general report endpoint to get and request params

--- src/main/java/com/lunark/lunark/reports/controller/ReportController.java
+++ src/main/java/com/lunark/lunark/reports/controller/ReportController.java
@@ -1,24 +1,20 @@
 package com.lunark.lunark.reports.controller;
 
 import com.lunark.lunark.auth.model.Account;
-import com.lunark.lunark.reports.dto.GeneralReportRequestDto;
 import com.lunark.lunark.reports.dto.GeneralReportResponseDto;
 import com.lunark.lunark.reports.dto.PropertyReportRequestDto;
 import com.lunark.lunark.reports.dto.PropertyReportResponseDto;
 import com.lunark.lunark.reports.model.GeneralReport;
 import com.lunark.lunark.reports.model.MonthlyReport;
 import com.lunark.lunark.reports.service.IReportService;
-import com.lunark.lunark.reservations.repository.IReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Collection;
 
 @RestController
@@ -31,12 +27,15 @@ public class ReportController {
         this.reportService = reportService;
     }
 
-    @PostMapping("/general")
+    @GetMapping("/general")
     @PreAuthorize("hasAuthority('HOST')")
-    public ResponseEntity<GeneralReportResponseDto> generateGeneralReport(@RequestBody GeneralReportRequestDto dto) {
+    public ResponseEntity<GeneralReportResponseDto> generateGeneralReport(
+            @RequestParam LocalDate start,
+            @RequestParam LocalDate end
+            ) {
         Account account = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        GeneralReport result = reportService.generateGeneralReport(dto.getStart(), dto.getEnd(), account.getId());
+        GeneralReport result = reportService.generateGeneralReport(start, end, account.getId());
 
         return new ResponseEntity<>(new GeneralReportResponseDto(result.getReservationCount(), result.getProfit()), HttpStatus.OK);
     }
