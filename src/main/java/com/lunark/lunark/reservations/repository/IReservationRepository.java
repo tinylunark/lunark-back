2054a8c940fe8de3afe3c1b9348a4a81fbb4421e
Vladislav Radovic <vradovic522@gmail.com>
2024-01-11 18:32:50 +0100

    feat: add query for property reports

--- src/main/java/com/lunark/lunark/reservations/repository/IReservationRepository.java
+++ src/main/java/com/lunark/lunark/reservations/repository/IReservationRepository.java
@@ -1,12 +1,15 @@
 package com.lunark.lunark.reservations.repository;
 
 import com.lunark.lunark.auth.model.Account;
+import com.lunark.lunark.reports.dto.PropertyReportResponseDto;
 import com.lunark.lunark.reservations.model.Reservation;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.Year;
 import java.util.Optional;
 
 import java.time.LocalDate;
@@ -21,4 +24,14 @@ public interface IReservationRepository extends JpaRepository<Reservation, Long>
     List<Reservation> findByPropertyId(Long propertyId);
     @Query("select r from Reservation r where r.guest.id = :guest_id and r.property.host.id = :host_id and r.status = 1 and r.endDate between :from and CURRENT_DATE")
     Collection<Reservation> findAllPastReservationsAtHostAfterDate(@Param("guest_id") Long guestId, @Param("host_id") Long hostId, @Param("from") LocalDate from);
+
+    @Query("select new com.lunark.lunark.reports.dto.PropertyReportResponseDto(extract(month from r.endDate), sum(r.price), count(*)) " +
+            "from Reservation r " +
+            "where extract(year from r.endDate) = :year " +
+            "and r.property.id = :property_id " +
+            "and r.endDate < current_date " +
+            "and r.status = 1 " +
+            "group by extract(month from r.endDate) " +
+            "order by extract(month from r.endDate) ")
+    Collection<PropertyReportResponseDto> generatePropertyReport(@Param("property_id") Long propertyId, @Param("year") Integer year);
 }
