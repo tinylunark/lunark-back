1758b98f61e41ffc5344207f5b157345a937a8af
Vladislav Radovic <vradovic522@gmail.com>
2024-01-11 18:51:23 +0100

    feat: add all args constructors

--- src/main/java/com/lunark/lunark/reports/model/GeneralReport.java
+++ src/main/java/com/lunark/lunark/reports/model/GeneralReport.java
@@ -1,11 +1,13 @@
 package com.lunark.lunark.reports.model;
 
 import com.lunark.lunark.reservations.model.Reservation;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Collection;
 
 @Data
+@AllArgsConstructor
 public class GeneralReport {
     Integer reservationCount;
     Double profit;
