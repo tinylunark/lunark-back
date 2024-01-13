d858c7ec80f94b54a344e0d88ee11953e3affa3d
Momir Milutinović <milutinm462@gmail.com>
2024-01-13 13:23:18 +0100

    fix: Invoke custom constraint validation exception handler when a request body is not valid

--- src/main/java/com/lunark/lunark/moderation/dto/AccountReportRequestDto.java
+++ src/main/java/com/lunark/lunark/moderation/dto/AccountReportRequestDto.java
@@ -1,10 +1,7 @@
 package com.lunark.lunark.moderation.dto;
 
 import com.lunark.lunark.validation.AccountExistsConstraint;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDateTime;
 
@@ -18,6 +15,7 @@ public record AccountReportRequestDto(
         Long reportedId,
         @NotNull
         @NotEmpty
+        @Pattern(message="Report reason can contain alphanumeric characters and punctuation marks only", regexp = "[a-zA-Z0-9 \\.!\\?]+")
         String reason
 ) {
 }
