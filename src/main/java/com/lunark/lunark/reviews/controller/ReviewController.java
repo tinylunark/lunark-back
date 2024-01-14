5bd7d99d0d3a361ccc26aa213c396279a6c91f93
Momir Milutinović <milutinm462@gmail.com>
2024-01-14 13:07:01 +0100

    feat: Add validations to ids in ReviewController

--- src/main/java/com/lunark/lunark/reviews/controller/ReviewController.java
+++ src/main/java/com/lunark/lunark/reviews/controller/ReviewController.java
@@ -6,6 +6,8 @@ import com.lunark.lunark.reviews.dto.*;
 import com.lunark.lunark.reviews.model.Review;
 import com.lunark.lunark.reviews.service.ReviewService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
@@ -14,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Clock;
@@ -24,6 +27,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/reviews")
+@Validated
 public class ReviewController {
 
     @Autowired
@@ -36,7 +40,7 @@ public class ReviewController {
     Clock clock;
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ReviewDto> getReview(@PathVariable("id") Long id) {
+    public ResponseEntity<ReviewDto> getReview(@PathVariable("id") @NotNull @PositiveOrZero Long id) {
         Optional<Review> review = reviewService.find(id);
         if (review.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -46,7 +50,7 @@ public class ReviewController {
     }
 
     @GetMapping(value = "/host/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<ReviewDto>> getReviewForHost(@PathVariable("id") Long id) {
+    public ResponseEntity<Collection<ReviewDto>> getReviewForHost(@PathVariable("id") @NotNull @PositiveOrZero Long id) {
         Collection<Review> reviews = reviewService.getAllReviewsForHost(id);
         if(reviews == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -55,7 +59,7 @@ public class ReviewController {
     }
 
     @GetMapping(value = "/property/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<ReviewDto>> getReviewForProperty(@PathVariable("id") Long id) {
+    public ResponseEntity<Collection<ReviewDto>> getReviewForProperty(@PathVariable("id") @NotNull @PositiveOrZero Long id) {
         Collection<Review> reviews = reviewService.getALlReviewsForProperty(id);
         if(reviews == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -65,7 +69,7 @@ public class ReviewController {
 
     @PreAuthorize("hasAuthority('GUEST')")
     @PostMapping(value = "/property/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ReviewDto> createPropertyReview(@Valid @RequestBody ReviewRequestDto reviewDto, @PathVariable(value = "id") Long id) {
+    public ResponseEntity<ReviewDto> createPropertyReview(@Valid @RequestBody ReviewRequestDto reviewDto, @PathVariable(value = "id") @NotNull @PositiveOrZero Long id) {
         Account guest = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (!this.reviewService.guestEligibleToReviewProperty(guest.getId(), id)) {
             return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
@@ -77,7 +81,7 @@ public class ReviewController {
 
     @PreAuthorize("hasAuthority('GUEST')")
     @PostMapping(value = "/host/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ReviewDto> createHostReview(@Valid @RequestBody ReviewRequestDto reviewDto, @PathVariable(value = "id") Long id) {
+    public ResponseEntity<ReviewDto> createHostReview(@Valid @RequestBody ReviewRequestDto reviewDto, @PathVariable(value = "id") @NotNull @PositiveOrZero Long id) {
         Account guest = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (!this.reviewService.guestEligibleToReviewHost(guest.getId(), id)) {
             return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
@@ -90,7 +94,7 @@ public class ReviewController {
 
     @DeleteMapping(value = "/{id}")
     @PreAuthorize("hasAuthority('GUEST')")
-    public ResponseEntity<Review> deleteReview(@PathVariable("id") Long id){
+    public ResponseEntity<Review> deleteReview(@PathVariable("id") @NotNull @PositiveOrZero Long id){
         Account guest = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (reviewService.find(id).isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -103,7 +107,7 @@ public class ReviewController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Review> updateReview(@RequestBody ReviewDto reviewDto, @PathVariable("id") Long id) {
+    public ResponseEntity<Review> updateReview(@Valid @RequestBody ReviewDto reviewDto, @PathVariable("id") @NotNull @PositiveOrZero Long id) {
         Optional<Review> existingReview = reviewService.find(id);
         if(existingReview.isEmpty()) {
            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -128,7 +132,7 @@ public class ReviewController {
 
     @PostMapping(value = "/{id}/approve", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAuthority('ADMIN')")
-    public ResponseEntity<ReviewApprovalDto> approveReview(@PathVariable("id") Long id) {
+    public ResponseEntity<ReviewApprovalDto> approveReview(@PathVariable("id") @NotNull @PositiveOrZero Long id) {
         Optional<Review> existingReview = reviewService.find(id);
         if(existingReview.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -143,7 +147,7 @@ public class ReviewController {
     }
 
     @GetMapping(value = "property-review-eligibility/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<PropertyReviewEligibilityDto> checkEligibilityToReviewProperty(@PathVariable("id") Long id) {
+    public ResponseEntity<PropertyReviewEligibilityDto> checkEligibilityToReviewProperty(@PathVariable("id") @NotNull @PositiveOrZero Long id) {
         Account guest;
         try {
             guest = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -155,7 +159,7 @@ public class ReviewController {
     }
 
     @GetMapping(value = "host-review-eligibility/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<HostReviewEligibilityDto> checkEligibilityToReviewHost(@PathVariable("id") Long id) {
+    public ResponseEntity<HostReviewEligibilityDto> checkEligibilityToReviewHost(@PathVariable("id") @NotNull @PositiveOrZero Long id) {
         Account guest;
         try {
             guest = (Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
