46257ff3aaa6cb8e26dd92f607de9fbc19ed2f47
Vladislav Radovic <vradovic522@gmail.com>
2024-01-19 20:44:33 +0100

    chore: add availability entries to test data

--- src/test/resources/test-data-availability.sql
+++ src/test/resources/test-data-availability.sql
@@ -8,3 +8,6 @@ insert into property_availability_entry(date, price, is_reserved, property_id) v
 insert into property_availability_entry(date, price, is_reserved, property_id) values ('2023-12-10', 2000, false, 1);
 insert into property_availability_entry(date, price, is_reserved, property_id) values ('2023-12-11', 2000, true, 1);
 insert into property_availability_entry(date, price, is_reserved, property_id) values ('2023-12-12', 2000, true, 1);
+insert into property_availability_entry(date, price, is_reserved, property_id) values ('2024-12-12', 2000, false, 1);
+insert into property_availability_entry(date, price, is_reserved, property_id) values ('2024-12-13', 2000, false, 1);
+insert into property_availability_entry(date, price, is_reserved, property_id) values ('2024-12-14', 2000, true, 1);
